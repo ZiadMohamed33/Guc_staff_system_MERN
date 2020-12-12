9ce19a960d67d757954fbb10046b22f27399fb21
zizo3333 <60374371+zizo3333@users.noreply.github.com>
2020-12-13 00:07:31 +0200

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -9,19 +9,33 @@ router.get('/',(req,res)=>{
     res.send("login page");
     });
  
-router.route('/login').post(async(req, res) => {
+router.route('/').post(async(req, res) => {
     const {email, password} = req.body
-    // TODO Validate the inputs required exist
+    
+//  Validate the inputs required exist
+    if(!email & !password )
+    {
+    return res.status(400).send("Email address field and password field are empty");
+    }
+    else if(!email)
+    {
+        return res.status(400).send("Email address field is empty");
+    }
+    else if(!password)
+    {
+        return res.status(400).send("password field is empty");
+    }
+    
    
     const user = await userModel.findOne({email: email});
     if(!user)
     {
-        return res.status(400).send('Email address doesn\'t exist');
+        return res.status(400).send("Invalid email or Password");
     }
     // TODO What if user not existing in database
     const correctPassword = await bcrypt.compare(password, user.password);
     if (!correctPassword) {
-    return res.status(400).send('Invalid Password');
+    return res.status(400).send('Invalid email or Password');
     }
     //const token = jwt.sign({_id: user.id, role: user.role}, process.env.TOKEN_SECRET);
     //res.header('token', token).send(token);
