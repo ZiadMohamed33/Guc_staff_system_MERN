d04b5f193a11ff9d3a4b7f29610b608a2d912d8b
ziad mohamed <zeyy32@yahoo.com>
2020-12-12 19:35:06 +0200

    login and signup routes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-//added signup
-// Added login 
+
 
 //express
 const express=require('express');
@@ -9,7 +8,24 @@ const app =express();
 const bodyParser=require("body-parser");
 app.use(bodyParser.json());
 
+//jwt
+const jwt=require('jsonwebtoken');
+app.use(express.json());
 
+
+  
+// jwt.sign(user,process.env.ACCESS_TOCKEN_SECRET)
+ 
+
+/*
+app.use(async (req, res, next) => {
+   const token = req.headers.token;
+   // TODO deny access if token does not exist
+   req.user = jwt.verify(token, process.env.TOKEN_SECRET);
+   next();
+  });
+  */
+ 
 //port connection
 const port=process.env.PORT || 2000 ;
 app.listen(port,()=>{
@@ -24,6 +40,8 @@ const connectionParams={
    useCreateIndex: true,
    useUnifiedTopology: true 
 }
+app.use(express.urlencoded({extended:false}));
+
 const URL = "mongodb+srv://zizo33:19989898@cluster0.hfmnd.mongodb.net/Stalin?retryWrites=true&w=majority";
 mongoose.connect(URL,connectionParams).then(()=>{
    console.log("Db connection succesfull");
@@ -32,13 +50,24 @@ mongoose.connect(URL,connectionParams).then(()=>{
 });
 
 
-//middleware
+//middlewares
+
+const Users = require('./models/Users');
 
 
 const router=require('./routes/Users');
-
 app.use('/Users',router);
-//check
+
+
+const login_route=require('./routes/login');
+app.use('/login',login_route);
+
+
+const signup_route=require('./routes/signup');
+app.use('/signup',signup_route);
+
+
+//check server status
 app.get('/',(req,res)=>{
    res.send("We are online");
    });
---
+++ routes/signup.js
@@ -0,0 +1,46 @@
+const express=require('express');
+const router = express.Router();
+const Users=require('../models/Users');
+
+
+
+//check signup page status
+router.get('/',(req,res)=>{
+    res.send("signup page");
+    });
+
+
+//signup
+router.post('/signup',async (req,res)=>{
+
+    const {email, password} = req.body
+   // TODO Validate the inputs required exist
+  
+   const user = await userModel.findOne({email: email});
+   // TODO What if user not existing in database
+
+
+    //authentication to be made
+    const username=req.body.name;
+       const new_user=new Users({
+          name:req.body.name,
+          password:req.body.password,
+          email:req.body.email,
+          role:req.body.role
+    });
+    try{
+       const saveduser=await new_user.save();
+       res.json(saveduser);    
+       
+       }catch(err){
+        res.json({message:err});
+       }
+   
+       //console.log(req.body);
+   
+ // jwt.sign(user,process.env.ACCESS_TOCKEN_SECRET)
+  
+ });
+ 
+
+module.exports=router;
