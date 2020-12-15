07e6bcf24ec2592545e6d47205817f3225e65fd5
zizo3333 <60374371+zizo3333@users.noreply.github.com>
2020-12-15 22:33:51 +0200

    users schema adjustments

--- models/Users.js
+++ models/Users.js
@@ -1,16 +1,24 @@
 const mongoose=require('mongoose');
 
 const UserSchema=mongoose.Schema({
-    name: {
+        name: {
         type: String, minlength: 4, maxlength: 30
         }, password: {
-        type: String,required: true,
+        type: String
         }, email: {
         type: String, required: true, lowercase: true
         }, role: {
+        type: Boolean, required: true,
+        }, age:  {
+        type: Number, min: 18, max: 90 
+        }, id:{
+        type: String, required: true,
+        }, gender:{
         type: String, required: true,
         }
+
        
 });
+
 
 module.exports=mongoose.model("Users",UserSchema);
