8b20e91b8892cc8e29222d3bd161ae1832b13e29
zizo3333 <60374371+zizo3333@users.noreply.github.com>
2020-12-19 16:27:00 +0200

    reset_password now running

--- routes/reset_password.js
+++ routes/reset_password.js
@@ -3,16 +3,26 @@ const router = express.Router();
 const userModel=require('../models/Users');
 const bcrypt = require('bcrypt');
 const jwt=require('jsonwebtoken');
-const Users = require('../models/Users');
 router.use(express.json());
 require('dotenv').config()
+const moment = require('moment');
 
 
 router.route('/').put(async(req, res) => {
 const id = req.body.id;
-const user = await userModel.find({id: id});
+let user = await userModel.find({id: id});
+
 if(req.body.password){
-return  user.password = req.body.password;
+   const salt = await bcrypt.genSalt(10);
+   req.body.password = await bcrypt.hash(req.body.password, salt);
+   let update=req.body.password;
+   console.log(req.body.password)
+   try{
+   let doc = await userModel.findOneAndUpdate({id: id}, {password:update});
+}catch(err){
+   res.json({message:err});
+}
+   res.send("password changed");
 }
    else{res.send('empty field')} 
 });
