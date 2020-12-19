f0dfa00e07c58a755dd0bbf8970651ed821c0314
mohamedelhendy <74493245+mohamedelhendy@users.noreply.github.com>
2020-12-19 12:28:14 +0200

    server

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
-//future second server
 const express = require('express');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
-
-
-
-
-
-//db connection
 const  DEV_PORT  = process.env.DEV_PORT;
 const  DB_URL  = process.env.DB_URL;
 console.log (DB_URL)
 const connectionOptions = {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true 
-
 };
 
-mongoose.connect(DB_URL, connectionOptions);
-
+mongoose.connect(DB_URL, connectionOptions).then(()=>{
+  console.log("Db connection succesfull");
+}).catch(()=>{
+  console.log("DB connection failed");
+})
+date = new Date()
+curMonth = date.getFullYear()
+console.log (curMonth)
 
 app.listen(DEV_PORT, () => {
   console.log(`[+] Server has started: http://localhost:${DEV_PORT}/`);
