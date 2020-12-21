76b147b916cdd432d0e4c03cf1c5f98aaceb28eb
zizo3333 <60374371+zizo3333@users.noreply.github.com>
2020-12-21 23:02:14 +0200

    arr_top to show latest elemnt in array pos

--- models/attendance.js
+++ models/attendance.js
@@ -1,4 +1,7 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
 
 
 const att_data = mongoose.Schema( {
@@ -7,9 +10,9 @@ const att_data = mongoose.Schema( {
    {timestamps: true });
 
 const attendance = mongoose.Schema({
-    id:{type: String, required: true},
-    day_off : {type: String},
+    id:{type: String, required: true,ref: 'Users'},
     Date: { type: Date, default: Date.now },
+    arr_top:{type:Number , default: "0"},
     att_data:[att_data],
     minutes_spent: {type:Number , default: "0"},
     monthly_minutes_spent : {type:Number , default: "0"}
@@ -17,6 +20,16 @@ const attendance = mongoose.Schema({
 
 
     
-    
 
-    module.exports=mongoose.model("attendance",attendance);
+function validate_attendance(attendance){
+    const schema = Joi.object({
+        id: Joi.string().required()
+
+
+
+    });
+    return schema.validate(attendance);
+}
+
+module.exports=mongoose.model("attendance",attendance);
+module.exports.validate_attendance = validate_attendance;
